9d62d9692cccbcfe08fa1f3b6bde1cc91f72e4a4
João Melo <joao.victor.ribeiro.melo@gmail.com>
2022-04-21 01:32:58 -0300

    Constroi esqueleto

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Navbar from "react-bootstrap/Navbar";
+
+export default function NavbarComponent(this: unknown) {
+    
+
+    return (
+        <Navbar>
+            
+        </Navbar>
+    );
+}
